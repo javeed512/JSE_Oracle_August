bdbd9e6725eac49764d14269ed14d22970c0b7f7
javeed512 <javeedmohammedjaveed@gmail.com>
2024-08-13 13:23:49 +0530

    Class,Object,Constructors,this,super,Unboxing,ObjectRefs Notes,Demos

---
+++ ClassAndObjects/src/com/oracle/Demo.java
@@ -0,0 +1,65 @@
+package com.oracle;
+
+import java.lang.*;
+
+public class Demo extends Object {
+
+	int id;
+	String name;
+
+	public Demo() {
+
+		super(); // immediate parent class , here Object();
+		// super --> it refers to immediate parent class object
+		System.out.println("Demo() is called..");
+
+	}
+
+	public Demo(int id, String name) {
+		// super(); // Object();
+		// parameter constructor is used to initialize instance variable values
+		this(); // this() refers to current class default constructor
+		this.id = id;
+		this.name = name; // this current class object
+		System.out.println("Demo() parameter constructor is called.");
+
+	}
+
+	public static void main(String[] args) {
+
+		// Demo d = new Demo();
+
+		// System.out.println(d.toString()); // @1243abe
+
+		// Object o = new Demo();
+
+		// System.out.println(o);
+
+		Demo d1 = new Demo(101, "Rohit");
+
+		System.out.println(d1);
+
+		// Demo.sayHello(); // static method call
+
+		Object o = new Demo();
+
+		// Demo.sayHello(new String("javeed"));
+
+		Demo.sayHello(d1);
+
+	}
+
+	public static  void  sayHello(Object obj) {  // concrete method
+		
+			Demo  d1 = (Demo) obj; // unboxing or explicit type casting
+			
+			
+			
+			//String s2 = (String)  obj;
+		
+		System.out.println("Hello Friends");
+		System.out.println(d1.id +" "+d1.name);
+		
+	}
+
+}
